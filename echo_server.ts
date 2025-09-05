c004928f91b5e48eb1c79aa4ec156313bcc6c1f3
mazode <mrmaazkhan191@gmail.com>
2025-09-05 23:15:48 +0300

    add: ending connection and receiving data functionality

--- echo_server.ts
+++ echo_server.ts
@@ -2,6 +2,16 @@ import * as net from "net";
 
 function newConn(socket: net.Socket): void {
   console.log("New connection: ", socket.remoteAddress, socket.remotePort);
+
+  // close connection
+  socket.on("end", () => {
+    console.log("End Of File.");
+  });
+
+  socket.on("data", (data: Buffer) => {
+    console.log("data", data);
+    socket.write(data); // echoing back the data
+  });
 }
 
 let server = net.createServer();
